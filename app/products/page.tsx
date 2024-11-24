490d64abce90b261f2512664542495711fdd65b8
DaniM0613 <wwh.rj.92@gmail.com>
2024-11-23 21:56:59 -0600

    Creando los contenedores principales

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,10 +1,10 @@
-import Sidebar from "@/components/sidebar"
+
 
 export default function ProductsPage(){
 
-    console.log('desde el servidor')
+  
 
     return (
-       <Sidebar/>
+        <h1>Product Page</h1>
     )
 }
