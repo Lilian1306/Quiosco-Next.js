92a6de6477f885c15caaaebecb5993b22d0aa812
DaniM0613 <wwh.rj.92@gmail.com>
2024-11-27 22:33:53 -0600

    Mostrando categorias y los iconos

--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import type { Category } from "@prisma/client"
 
 type CategoryIconProps = {
@@ -6,8 +7,19 @@ type CategoryIconProps = {
 
 export default function Categoryicon({category}: CategoryIconProps ) {
   return (
-    <div>
-      {category.name}
+    <div  className={` flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}>
+
+     <div className='w-16 h-16 relative'>
+      <img 
+        width={64}
+        height={64}
+        src={`/icon_${category.slug}.svg`} alt='Imagen categoria'/>
+       </div>
+
+       <p className='text-xl font-bold'>
+        {category.name}
+       </p>
+
     </div>
   )
 }
