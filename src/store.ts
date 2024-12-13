aea610989fbff8a803e44bf6fe4a98d76ca99d94
DaniM0613 <wwh.rj.92@gmail.com>
2024-12-13 16:46:00 -0600

    evitar ordenes duplicadas

--- src/store.ts
+++ src/store.ts
@@ -8,6 +8,7 @@ interface Store {
     increaseQuantity: (id: Product['id']) => void
     decreaseQuantity: (id: Product['id']) => void
     removeItem: (id: Product['id']) => void
+    clearOrder: () => void // Reiniciar la orden
 }
 
 export const useStore = create<Store>((set, get) => ({
@@ -56,6 +57,12 @@ export const useStore = create<Store>((set, get) => ({
     removeItem: (id) => {
         set((state) => ({
            order: state.order.filter(item => item.id !== id)
+        }))
+    },
+    // Reiniciar la orden
+    clearOrder: () => {
+        set(() => ({
+            order: []
         }))
     }
 
