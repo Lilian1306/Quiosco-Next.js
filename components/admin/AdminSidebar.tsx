a606e5f26f11691c233e5ab7937002f87411ab9b
DaniM0613 <wwh.rj.92@gmail.com>
2024-12-14 22:41:23 -0600

    Navegacion en el panel de administracion

--- components/admin/AdminSidebar.tsx
+++ components/admin/AdminSidebar.tsx
@@ -1,4 +1,5 @@
 import Logo from "../ui/Logo"
+import AdminRouter from "./AdminRouter"
 
 const adminNagivation = [ 
     {url: '/admin/orders', text: 'Ordenes', blank: false},
@@ -14,7 +15,12 @@ export default function AdminSidebar() {
       <div className="space-y-3">
         <p className="mt-10  uppercase font-bold text-sm text-gray-600 text-center">Navegacion</p>
          <nav className="flex flex-col">
-
+            {adminNagivation.map(link => ( 
+              <AdminRouter
+                 key={link.url}
+                 link={link}
+              />
+            ))}
          </nav>
 
       </div>
