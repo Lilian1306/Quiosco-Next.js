d4ea14d0e98ed10690f3717758c24e7a2ff25ff6
DaniM0613 <wwh.rj.92@gmail.com>
2024-12-18 22:34:10 -0600

    Listando los productos

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,11 +1,26 @@
 
+import ProductTable from '@/components/products/ProductsTable'
 import Heading from '@/components/ui/Heading'
+import { prisma } from '@/src/lib/prisma'
 
+async function getProducts(){
+  const products = await prisma.product.findMany()
 
-export default function page() {
+  return products
+}
+
+export default async function ProductsPage() {
+
+  const products = await getProducts()
+  console.log(products)
+ 
   return (
     <>
      <Heading>Administrar Productos</Heading>
+
+      <ProductTable
+         products={products}
+      />
       
     </>
   )
