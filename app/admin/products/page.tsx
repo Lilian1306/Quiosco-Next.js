c88ccce1f45b2211329434563f79a82c0c164ffe
DaniM0613 <wwh.rj.92@gmail.com>
2024-12-19 23:48:04 -0600

    Añadir paginador por numeros

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -31,14 +31,11 @@ export default async function ProductsPage({searchParams} : {searchParams: {page
 
   const page = +(params.page || 1)
   const pageSize = 10
-
-  if(page < 0 ||) redirect('/admin/products')
-
+  if(page < 0 ) redirect('/admin/products')
   const productsData =  getProducts(page, pageSize)
   const totalProductsData =  productCount()
   const [ products, totalProducts ] = await Promise.all([productsData, totalProductsData])
   const totalPages = Math.ceil(totalProducts / pageSize)
-
   if(page > totalPages) redirect('/admin/products')
   
 
