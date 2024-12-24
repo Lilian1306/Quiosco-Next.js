5009f9c49e31898c4850010c2e3b94abc4d203c3
DaniM0613 <wwh.rj.92@gmail.com>
2024-12-23 22:26:11 -0600

    Creando el componente para crear imagenes

--- components/products/ImageUpload.tsx
+++ components/products/ImageUpload.tsx
@@ -1,11 +1,41 @@
-
-
-import React from 'react'
+"use client"
+import { CldUploadWidget } from "next-cloudinary";
+import { TbPhotoPlus } from "react-icons/tb";
 
 export default function ImageUpload() {
   return (
-    <div>
-      ImageUpload
-    </div>
+    <CldUploadWidget
+        onSuccess={(result, { widget }) => {
+          console.log(result)
+        }}
+        uploadPreset='z6ujpdnw'
+        options={{
+          maxFiles: 1
+        }}
+        onUpload={(error, result) => {
+            if(error) {
+                console.log("Error uploading image:", error)
+            }else {
+                console.log("Image uploaded:", result)
+            }
+        }}
+    >
+       {({open}) => (
+        <>
+          <div className='space-y-2'>
+            <label className='text-slate-800' >Image Producto</label>
+            <div 
+               className='relative cursor-pointer hoever:opacity-701 transition p-10 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600 bg-slate-100'
+               onClick={() => open()}
+            > 
+            <TbPhotoPlus 
+               size={50}
+            />
+              <p className='text-lg font-semibold'>Agregar Imagen</p>
+              </div>
+          </div>
+        </>
+       )}
+    </CldUploadWidget>
   )
 }
