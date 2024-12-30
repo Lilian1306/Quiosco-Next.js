3124df6446a51fc1026c05ef11ad6a708bce1765
DaniM0613 <wwh.rj.92@gmail.com>
2024-12-30 17:04:42 -0600

    Revalidacion manual

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,6 +1,7 @@
 import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
+import { revalidatePath } from "next/cache";
 
 async function getPendingOrders() {
   const orders = await prisma.order.findMany({
@@ -22,10 +23,28 @@ async function getPendingOrders() {
 
 export default async function page() {
   const orders = await getPendingOrders()
+
+  const refreshOrders = async () => {
+    "use server"
+    revalidatePath('/admin/orders')
+  }
   
   return (
     <>
      <Heading>Administrar Ordenes</Heading>
+
+     <form
+        action={refreshOrders}
+     >
+
+      <input
+         type="submit"
+         value="Actualizar Ordenes"
+         className="bg-ambeer-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer"
+      />
+     </form>
+
+
       {orders.length ? (
         <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5"> 
          {orders.map(order => (
