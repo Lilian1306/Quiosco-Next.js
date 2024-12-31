4b10c0b22d915797fa79a8bd47a61ad3984eb9cf
DaniM0613 <wwh.rj.92@gmail.com>
2024-12-30 21:51:37 -0600

    Configurando SWR

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,53 +1,30 @@
+"use client"
+import useSWR from "swr";
 import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
-import { prisma } from "@/src/lib/prisma";
-import { revalidatePath } from "next/cache";
+import { OrderWithProducts } from "@/src/types";
 
-async function getPendingOrders() {
-  const orders = await prisma.order.findMany({
-    where: {
-      status: false
-    },
-    include: {
-      orderProducts: {
-        include: {
-          product: true
-        }
-      }
-    }
+
+
+export default  function page() {
+  const url = '/admin/orders/api'
+  const fetcher = () => fetch(url).then(res => res.json()).then( data=> data)
+  const {data, error, isLoading} = useSWR<OrderWithProducts[]>(url, fetcher, {
+     refreshInterval: 60000,
+     revalidateOnFocus: false
   })
 
-  return orders
-
-}
-
-export default async function page() {
-  const orders = await getPendingOrders()
-
-  const refreshOrders = async () => {
-    "use server"
-    revalidatePath('/admin/orders')
-  }
+  if(isLoading) return 'Cargando...'
   
-  return (
+  if(data) return (
     <>
      <Heading>Administrar Ordenes</Heading>
 
-     <form
-        action={refreshOrders}
-     >
+    
 
-      <input
-         type="submit"
-         value="Actualizar Ordenes"
-         className="bg-ambeer-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer"
-      />
-     </form>
-
-
-      {orders.length ? (
+      {data.length ? (
         <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5"> 
-         {orders.map(order => (
+         {data.map(order => (
           <OrderCard
             key={order.id}
             order={order}
